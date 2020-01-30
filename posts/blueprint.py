3659b4d4c4bc511fe8e1ff2b597c3f7afca34e18
Oksana <ksenia.barshchyk@yandex.ru>
2020-01-30 22:30:47 +0200

    added /create posts functionality

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -3,16 +3,31 @@ from flask import render_template
 from models import Post, Tag
 from .forms import PostForm
 from flask import request
+from app import db
+from flask import redirect
+from flask import url_for
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
-
 # http local host blog create
-@posts.route('/create')
+@posts.route('/create', methods=['POST', 'GET'])
 def create_post():
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print("Something wrong!")
+
+        return redirect(url_for('posts.index'))
+
     form = PostForm()
-    return render_template('/posts/create_post.html', form=form)
+    return render_template('posts/create_post.html', form=form)
 
 @posts.route('/')
 def index():
@@ -20,6 +35,7 @@ def index():
     if q:
         posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
     else:
+        # posts = Post.query.order_by(Post.created.desc)
         posts = Post.query.all()
 
     return render_template('posts/index.html', posts=posts)
