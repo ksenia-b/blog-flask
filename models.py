2fb38b87d1de1e8bf5099441d58008cdaa1bb7db
Oksana <ksenia.barshchyk@yandex.ru>
2020-02-04 23:17:05 +0200

    added Role and User models

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from app import db
 from datetime import datetime
 from sqlalchemy import DateTime
 import re
+from flask_security import UserMixin, RoleMixin
 
 def slugify(s):
     pattern = r'[^\w+]'
@@ -47,3 +48,22 @@ class Tag(db.Model):
 
     def __repr__(self):
         return '{}'.format(self.name)
+
+
+### falsk security
+roles_users = db.Table('role_users',
+                      db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                      db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+                                )
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
