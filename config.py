3ce93eb530c3fadfa36c5b573902b8405234cf1b
Oksana <ksenia.barshchyk@yandex.ru>
2020-02-04 22:37:17 +0200

    added admin panel

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ class Configuration(object):
     DEBUG = True
     SQL_ALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://admin:admin@localhost/test1'
+    SECRET_KEY = 'DLFNOSFHNKDFHKSSAIOH'
+
 
