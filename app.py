06f46062ecf1c1d38c71fd5b5f4c3aeb4678bfb9
Oksana <ksenia.barshchyk@yandex.ru>
2020-02-05 23:59:58 +0200

    added security

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_security import SQLAlchemyUserDatastore
 from flask_security import Security
+from flask_security import current_user
+from flask import redirect, url_for, request
+from flask_admin import AdminIndexView
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -19,9 +23,39 @@ manager.add_command('db', MigrateCommand)
 
 # Admin
 from models import *
-admin = Admin(app)
-admin.add_view(ModelView(Post, db.session))
-admin.add_view(ModelView(Tag, db.session))
+
+class BaseModelView(ModelView):
+    def on_model_change(self, form, model, is_created):
+        model.generate_slug()
+        return super(BaseModelView).on_model_change(form, model, is_created)
+
+class AdminMixin:
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('security.login', next=request.url))
+
+
+class AdminView(AdminMixin, ModelView):
+   pass
+
+class HomeAdminView(AdminIndexView):
+    pass
+
+
+class PostAdminView(AdminMixin, BaseModelView):
+    form_columns = ['title', 'body', 'tags']
+
+
+class TagAdminView(AdminMixin, BaseModelView):
+    from_columns = ['name', 'posts']
+
+
+admin = Admin(app, "FlaskApp", url='/', index_view=HomeAdminView(name='Home'))
+admin.add_view(AdminView(Post, db.session))
+admin.add_view(AdminView(Tag, db.session))
+
 
 # flask security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
